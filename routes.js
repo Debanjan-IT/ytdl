474bf25536a120c7fe5bea840b4df281a0596b6b
Debanjan Dasgupta <69705770+Debanjan-IT@users.noreply.github.com>
2022-07-27 19:06:41 +0530

    Add files via upload

---
+++ routes.js
@@ -0,0 +1,29 @@
+const Joi = require("joi");
+
+const tags = ["api", "data"];
+const controller = require('../controller')
+
+module.exports = {
+    name: "data",
+    register: (server, options) => {
+        const routes = [
+            {
+                method: "GET",
+                path: "/get-data",
+                config: {
+                    auth: false,
+                    description: "get data",
+                    tags,
+                    handler: controller.getData,
+                    validate: {
+                        query: Joi.object({
+                            yt_url: Joi.string().required(),
+                            type: Joi.string().valid('audio', 'video', 'all').default('all').allow(null)
+                        })
+                    },
+                },
+            }
+        ]
+        server.route(routes);
+    }
+}
---
+++ server.js
@@ -0,0 +1,51 @@
+'use strict';
+require('dotenv/config')
+const Hapi = require('@hapi/hapi');
+const Inert = require('@hapi/inert');
+const Vision = require('@hapi/vision');
+const HapiSwagger = require('hapi-swagger');
+const Pack = require('./package');
+const routes = require('./routes')
+
+const init = async () => {
+    const server = Hapi.server({
+        port: process.env.PORT || 3006,
+        routes: {
+          cors: true,
+        },
+    });
+    const swaggerOptions = {
+        info: {
+            title: 'API Documentation',
+            version: Pack.version,
+        },
+        basePath: '/api/',
+        documentationPath: '/api/documentation',
+        schemes: ['https', 'http'],
+    };
+
+    await server.register([
+        Inert,
+        Vision,
+        {
+            plugin: HapiSwagger,
+            options: swaggerOptions
+        }
+    ]);
+    await server.register(routes, {
+        routes: {
+            prefix: "/api"
+        }
+    });
+    try {
+        await server.start();
+        console.log('Server running at:', server.info.uri);
+    } catch(err) {
+        console.log(err);
+    }
+};
+process.on('unhandledRejection', (err) => {
+    console.log(err);
+    process.exit(1);
+});
+init();
